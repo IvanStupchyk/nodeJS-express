bfd6019dc856e6aa08fbea6c7167b56498f8250f
IvanStupchuk <hardtrynew@rambler.ru>
2022-05-16 17:57:22 +0200

    NJ-7 updated cart, added users cart

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const cardRoutes = require('./routes/card')
 const coursesRoutes = require('./routes/courses')
+const User = require('./models/user')
+
 const app = express()
 
 const hbs = exphbs.create({
@@ -20,12 +22,25 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('62825fc4043f1d0e8d897db0')
+
+    req.user = user
+    next()
+  } catch (e) {
+    console.log(e)
+  }
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
+
+
 
 const PORT = process.env.PORT || 3000
 
@@ -36,6 +51,20 @@ async function start() {
     await mongoose.connect(URL, {
       useNewUrlParser: true,
     })
+
+    const candidate = await User.findOne()
+
+    if (!candidate) {
+      const user = new User({
+        email: 'valyastupchuk@gmail.com',
+        name: 'Ivan',
+        card: {
+          items: []
+        }
+      })
+
+      await user.save()
+    }
 
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
