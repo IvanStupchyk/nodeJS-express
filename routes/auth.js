4cba68154baaee9b706f0efee729ba15fea9c196
IvanStupchuk <hardtrynew@rambler.ru>
2022-05-18 12:18:53 +0200

    NJ-10 added route protection, password encryption

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const {Router} = require('express')
 const router = new Router()
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
 router.get('/login', async (req, res) => {
@@ -16,15 +17,57 @@ router.get('/logout', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-  req.session.user = await User.findById('62825fc4043f1d0e8d897db0')
-  req.session.isAuthenticated = true
-  req.session.save(err => {
-    if (err) {
-      throw err
+  try {
+    const {email, password} = req.body
+
+    const candidate = await User.findOne({email})
+
+    if (candidate) {
+      const isSame = await bcrypt.compare(password, candidate.password)
+
+      if (isSame) {
+        req.session.user = candidate
+        req.session.isAuthenticated = true
+        req.session.save(err => {
+          if (err) {
+            throw err
+          } else {
+            res.redirect('/')
+          }
+        })
+      } else {
+        res.redirect('/auth/login#login')
+      }
     } else {
-      res.redirect('/')
+      res.redirect('/auth/login#login')
     }
-  })
+  } catch (e) {
+    console.log(e)
+  }
+
+})
+
+router.post('/register', async (req, res) => {
+  try {
+    const {email, password, repeat, name} = req.body
+
+    const candidate = await User.findOne({email})
+
+    if (candidate) {
+      res.redirect('/auth/login#register')
+    } else {
+      const hashPassword = await bcrypt.hash(password, 10)
+
+      const user = new User({
+        email, name, password: hashPassword, cart: {items: []}
+      })
+
+      await user.save()
+      res.redirect('/auth/login#login')
+    }
+  } catch (e) {
+    console.log(e)
+  }
 })
 
 module.exports = router
